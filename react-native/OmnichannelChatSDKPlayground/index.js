bc977f696d2f37c8de9267fb3df2b9e817d9a893
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-06-19 17:49:59 -0700

    Install dependencies & Configure project to work with @microsoft/omnichannel-chat-sdk

--- react-native/OmnichannelChatSDKPlayground/index.js
+++ react-native/OmnichannelChatSDKPlayground/index.js
@@ -2,6 +2,10 @@
  * @format
  */
 
+import 'node-libs-react-native/globals';
+import 'react-native-get-random-values';
+import 'react-native-url-polyfill/auto';
+import '@azure/core-asynciterator-polyfill';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
