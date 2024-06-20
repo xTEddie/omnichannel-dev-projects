f249357fa8654a68f903db342112847ed447f2a5
Edward Tran (HE/HIM) (from Dev Box) <edtran@microsoft.com>
2024-06-19 17:54:55 -0700

    Add functionalities to buttons

--- react-native/OmnichannelChatSDKPlayground/App.tsx
+++ react-native/OmnichannelChatSDKPlayground/App.tsx
@@ -5,7 +5,8 @@
  * @format
  */
 
-import React, { useCallback } from 'react';
+import { OmnichannelChatSDK } from '@microsoft/omnichannel-chat-sdk';
+import React, { useCallback, useEffect, useState } from 'react';
 import {
   Button,
   SafeAreaView,
@@ -20,23 +21,65 @@ import {
 } from 'react-native/Libraries/NewAppScreen';
 
 function App(): React.JSX.Element {
+  const [chatSDK, setChatSDK] = useState();
+
   const isDarkMode = useColorScheme() === 'dark';
 
   const backgroundStyle = {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
+  useEffect(() => {
+    console.log("@azure/communication-common");
+    console.log(require('@azure/communication-common/package.json').version);
+    console.log("@azure/communication-chat");
+    console.log(require('@azure/communication-chat/package.json').version);
+    console.log("@azure/communication-signaling");
+    console.log(require('@azure/communication-signaling/package.json').version);
+  }, []);
+
+  useEffect(() => {
+    const init = async () => {
+      console.log("[init]");
+
+      const omnichannelConfig = {
+        orgId: '',
+        orgUrl: '',
+        widgetId: ''
+      };
+
+      const chatSDK = new OmnichannelChatSDK(omnichannelConfig);
+      setChatSDK(chatSDK as any);
+
+      // chatSDK.setDebug(true);
+      await chatSDK.initialize();
+    }
+
+    init();
+  }, []);
+
   const startChat = useCallback(async () => {
     console.log("[startChat]");
-  }, []);
+    await (chatSDK as any).startChat();
+
+    (chatSDK as any).onNewMessage((message: any) => {
+      console.log("New Message");
+      console.log(message);
+    })
+
+  }, [chatSDK]);
 
   const sendMessage = useCallback(async () => {
     console.log("[sendMessage]");
-  }, []);
+    await (chatSDK as any).sendMessage({
+      content: "hi"
+    });
+  }, [chatSDK]);
 
   const endChat = useCallback(async () => {
     console.log("[endChat]");
-  }, []);
+    await (chatSDK as any).endChat();
+  }, [chatSDK]);
 
   return (
     <SafeAreaView style={backgroundStyle}>
